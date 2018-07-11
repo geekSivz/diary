6b0556826c991c3e6ed75744f9ed014d4a11a82b
siva <g.sivanraj@gmail.com>
2018-07-10 22:11:46 -0700

    polishing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -19,6 +19,7 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     MDBBootstrapModule.forRoot()
   ],
   schemas: [  ],
--- client/src/app/diaryadd/diaryadd.component.ts
+++ client/src/app/diaryadd/diaryadd.component.ts
@@ -2,20 +2,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Diary } from './diary.model'
 import { CommonService } from '../common/common.service'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'diary-add',
     templateUrl: './diaryadd.component.html',
-    styleUrls: ['./diaryadd.component.css']
+    styleUrls: ['./diaryadd.component.scss']
 
 })
 export class DiaryAddComponent implements OnInit {
 
-    private diaryItem
+    private diaryItem;
 
+    modalForm: FormGroup;
 
-    constructor(private commonService:CommonService) {
-
+    constructor(private commonService:CommonService, public fb: FormBuilder) {
+        this.modalForm = fb.group({
+            modalFormNameEx: ['', Validators.required],
+            modalFormEmailEx: ['', [Validators.email, Validators.required]],
+            modalFormSubjectEx: ['', Validators.required],
+            modalFormTextEx: ['', Validators.required]
+          });
+      
     }
 
     addDiary(){
